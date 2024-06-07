31586e7fe2e15471ee3b4a923b700e4367f52eb8
Saif <saifbenyaagoub@gmail.com>
2024-06-07 17:22:32 +0200

    day1

--- app/page.tsx
+++ app/page.tsx
@@ -1,55 +1,43 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from './style.module.css'; // Assuming you have a CSS module or global CSS file
+import styles from './style.module.css';
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <link rel="stylesheet" href="style.css" />
         <title>Inscription</title>
       </Head>
-      <form action="">
-        <div id="d1">
-          <p>
-            <Image src="/im1.png" alt="Inscription" width={50} height={50} />
-            Inscription
-          </p>
-        </div>
-        <table>
-          <tbody>
-            <tr>
-              <td><label>Entrer votre prénom :</label></td>
-              <td><input type="text" id="c1" /><br /></td>
-            </tr>
-            <tr>
-              <td><label>Votre classe :</label></td>
-              <td>
-                <select id="c2">
-                  <option value="Seconde 1">Seconde 1</option>
-                  <option value="Seconde 2">Seconde 2</option>
-                  <option value="Seconde 3">Seconde 3</option>
-                  <option value="Seconde 4">Seconde 4</option>
-                </select>
-              </td>
-            </tr>
-            <tr>
-              <td><label>votre E-mail :</label></td>
-              <td><input type="email" id="c3" /></td>
-            </tr>
-            <tr>
-              <td><label>Votre pswd
-                 :</label></td>
-              <td><input type="password" id="c4" /></td>
-            </tr>
-            <tr>
-              <td colSpan={2}>
-                <input type="submit" className="submit" id="sub" value="S'INSCRIRE" />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </form>
-    </div>
+      <body>
+        <header>
+          <div className="contain">
+            <Image src="/Saif.png" alt="Logo" width={100} height={100} />
+            <ul>
+              <li><a href=""> Home</a></li>
+              <li><a href=""> About</a></li>
+              <li><a href=""> Portfolio</a></li>
+              <li><a href=""> Contact</a></li>
+            </ul>
+          </div>
+          <div className="headtext">
+            <h1>Hi, I'm Saif</h1>
+            <Image src="/giphy.gif" alt="Animated GIF" width={300} height={200} />
+          </div>
+          <div id="about">
+            <div className="contain">
+              <div className="row">
+                <div className="col1">
+                  <Image src="/bg.jpg" alt="Background" width={400} height={300} />
+                </div>
+                <div className="col2">
+                  <h1>About Me</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+      </body>
+    </>
   );
 }
