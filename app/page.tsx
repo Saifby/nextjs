eba3a118a4e3e06e46d0e92c8a61bf4e7da50246
Saif <saifbenyaagoub@gmail.com>
2024-06-10 12:57:16 +0200

    day2

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import '../styles/globals.css'; 
+import styles from './style.module.css'; 
 
 export default function Home() {
   return (
@@ -14,24 +14,32 @@ export default function Home() {
           <ul>
             <li><a href=""> Home</a></li>
             <li><a href=""> About</a></li>
-            <li><a href=""> Portfolio</a></li>
+            <li><a href=""> Service</a></li>
             <li><a href=""> Contact</a></li>
           </ul>
         </div>
+        </header>
         <div className="headtext">
-          <h1>Hi, I&apos;m Saif</h1>
-          <Image src="/giphy.gif" alt="Animated GIF" width={300} height={200} />
+          <h1>Hi, I&apos;m Saif  <img src="bg.jpg" alt="" /></h1><br />
         </div>
+
         <div id="about">
           <div className="contain">
             <div className="row">
               <div className="col1">
-                <h1>About Me</h1>
+              <Image src="/img1.jpg" alt="Background" width={400} height={300} />
+              </div>
+              <div className="col2">
+              <h1>About Me</h1>
+                <p>20-year-old programmer fueled by passion and curiosity. Driven by a thirst for knowledge, I immerse myself in the world of coding, constantly seeking to learn and innovate.  </p>
+              <div className='titles'>
+                <a href="cv.pdf"><button>CV</button></a>
+              </div>
               </div>
             </div>
           </div>
         </div>
-      </header>
+      
     </div>
   );
 }
