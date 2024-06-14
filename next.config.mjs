c3be689c35da73c70d5e8b8d6634995256e1193c
Saif <saifbenyaagoub@gmail.com>
2024-06-14 12:11:19 +0200

    web

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: "/saif-by",
-  // other configuration options
-  reactStrictMode: true,
-};
+const nextConfig = {};
 
 export default nextConfig;
